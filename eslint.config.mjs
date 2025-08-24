30d300faafc300e78f9380f07166d2ba9168bc44
andrei-myshoves <andreimyshoves89@gmail.com>
2025-08-24 11:59:39 +0200

    test line-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,49 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import json from '@eslint/json';
-import markdown from '@eslint/markdown';
-import css from '@eslint/css';
-import prettier from 'eslint-plugin-prettier';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import css from '@eslint/css'
+import prettier from 'eslint-plugin-prettier'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js, prettier },
-    rules: {
-      'prettier/prettier': 'error',
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js, prettier },
+        extends: ['js/recommended'],
+        rules: {
+            'prettier/prettier': 'error',
+            'arrow-parens': ['error', 'as-needed'], 
+            'arrow-body-style': ['error', 'as-needed'],
+        },
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node },
+        },
     },
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
     },
-  },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
-  {
-    files: ['**/*.jsonc'],
-    plugins: { json },
-    language: 'json/jsonc',
-    extends: ['json/recommended'],
-  },
-  {
-    files: ['**/*.json5'],
-    plugins: { json },
-    language: 'json/json5',
-    extends: ['json/recommended'],
-  },
-  {
-    files: ['**/*.md'],
-    plugins: { markdown },
-    language: 'markdown/gfm',
-    extends: ['markdown/recommended'],
-  },
-  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
-]);
+    { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+    {
+        files: ['**/*.jsonc'],
+        plugins: { json },
+        language: 'json/jsonc',
+        extends: ['json/recommended'],
+    },
+    {
+        files: ['**/*.json5'],
+        plugins: { json },
+        language: 'json/json5',
+        extends: ['json/recommended'],
+    },
+    {
+        files: ['**/*.md'],
+        plugins: { markdown },
+        language: 'markdown/gfm',
+        extends: ['markdown/recommended'],
+    },
+    { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
+])

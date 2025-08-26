0ad0daa8da7f962b453df83dd04b41062670e5a9
andrei-myshoves <andreimyshoves89@gmail.com>
2025-08-26 08:50:46 +0200

    resolve back button now in heder

--- src/components/PageBlock/PageBlock.js
+++ src/components/PageBlock/PageBlock.js
@@ -3,8 +3,13 @@ import * as styles from './PageBlock.module.css'
 export function PageBlock(title) {
     return `
     <div class="${styles.page}">
-      <h2>${title}</h2>
-      <button id="backBtn" class="${styles.button}">Назад</button>
+      <header class="${styles.header}">
+        <button id="backBtn" class="${styles.button}">Назад</button>
+        <h2>${title}</h2>
+      </header>
+      <div class="${styles.content}">
+        <!-- Тут будет контент страницы -->
+      </div>
     </div>
   `
 }

5484af2396c25610958039d3798c8d1f1451e637
andrei-myshoves <andreimyshoves89@gmail.com>
2025-09-21 09:05:41 +0200

    BEK-5:add container  in layout.module.css and repair style in gamepage.module.css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import { Header } from '../Header/Header.js'
 import { htmlToElement } from '../../utils/htmlToELement.js'
+import * as styles from './Layout.module.css'
 
 export function Layout({ title = '', children = '', showBack = false }) {
     return htmlToElement(`
-    <div class="layout">
-      ${Header(title, showBack).outerHTML}
-      <div class="content">
-        ${children instanceof HTMLElement ? children.outerHTML : children}
+    <div class="${styles.container}">
+      <div class="layout">
+        ${Header(title, showBack).outerHTML}
+        <div class="content">
+          ${children instanceof HTMLElement ? children.outerHTML : children}
+        </div>
       </div>
     </div>
   `)

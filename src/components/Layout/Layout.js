3511a524e2d38d1f059bf114cd2774b2289ce7ac
andrei-myshoves <andreimyshoves89@gmail.com>
2025-10-01 20:10:59 +0200

    BEK-5:resolve commit for layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,15 +4,19 @@ import * as styles from './Layout.module.css'
 
 export function Layout({ title = '', children = '', showBack = false }) {
     const layoutEl = htmlToElement(`
-      <div class="${styles.container}">
-        <div class="${styles.layout}">
-          ${Header(title, showBack).outerHTML}
-          <div id="childrenContainer"></div>
-        </div>
+    <div class="${styles.container}">
+      <div class="${styles.layout}">
+        ${Header(title, showBack).outerHTML}
+        <div id="childrenContainer"></div>
       </div>
-    `)
+    </div>
+  `)
 
-    const childrenContainer = layoutEl.querySelector('#childrenContainer')
+    layoutEl.getElementById = function (id) {
+        return this.querySelector(`#${id}`)
+    }
+
+    const childrenContainer = layoutEl.getElementById('childrenContainer')
 
     if (childrenContainer) {
         if (children instanceof HTMLElement) {
